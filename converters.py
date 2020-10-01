757f8827d39a7e6d78d094e360c11c35840cfeb0
abbymrobinson <70149779+abbymrobinson@users.noreply.github.com>
2020-10-01 15:02:19 -0400

    first working version

--- converters.py
+++ converters.py
@@ -1,2 +1,5 @@
-# output 25mpg into kpl
-print(25 * 1.609344 * 0.2641720524)
+# ask user to input MPG value
+mpg = input("What is the MPG?")
+# Convert the input into a numeric value
+mpg = float(mpg)
+print(mpg * 1.609344 * 0.2641720524)
